886fdb4a017237310450075bc9e0f20206ea2da8
MyraMidnight <dark_stardragon@hotmail.com>
2018-11-10 17:08:13 +0000

    header navigation works

--- src/lib/components/header.js
+++ src/lib/components/header.js
@@ -6,18 +6,17 @@ import React from 'react';
 import * as actions from '../../actions';
 
 class Header extends React.Component {
-    handleWikiButton = (page)=>{
-        return this.props.changeCurrentPage(page)
-        
+    handleButtonClick = (page)=>{
+        this.props.changeCurrentPage({page: page});
     }
     render() {
         return (
             <div>
                 <h2> Header</h2>
                 <nav>
-                    <button type="button" onClick={this.handleWikiButton}> Wiki </button>
-                    <button type="button" onClick={this.handleWikiButton}> Wiki </button>
-                    <button type="button" onClick={this.handleWikiButton}> Wiki </button>
+                    <button type="button" onClick={()=> this.handleButtonClick("page-wiki")}> Wiki </button>
+                    <button type="button" onClick={()=> this.handleButtonClick("page-news")}> news </button>
+                    <button type="button" onClick={()=> this.handleButtonClick("page-main")}> Main </button>
                 </nav>
             </div>
         )
