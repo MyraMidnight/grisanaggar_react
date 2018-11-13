5af1382964cb6b675c4c0a8e4f6ad8f3ce479099
Ellert Smári Kristbergsson <ellertsmari@gmail.com>
2018-11-13 13:51:46 +0000

    bætti við relative path

---
+++ build/precache-manifest.811f58d33ee4aeb6e9ec5fb85ead44ef.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "98e92ad24507b1c1c121",
+    "url": "/prototype/build/static/js/runtime~main.98e92ad2.js"
+  },
+  {
+    "revision": "7efc5f02f53d50f42e41",
+    "url": "/prototype/build/static/js/main.7efc5f02.chunk.js"
+  },
+  {
+    "revision": "3ff2d454d35e2f092886",
+    "url": "/prototype/build/static/js/1.3ff2d454.chunk.js"
+  },
+  {
+    "revision": "42ad09228930362a092bf0472f23e20b",
+    "url": "/prototype/build/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e74b9c6645703c65fc542d585a41e898.js"
+  "/prototype/build/precache-manifest.811f58d33ee4aeb6e9ec5fb85ead44ef.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/prototype/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
