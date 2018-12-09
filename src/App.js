18b750a29a6d2aeb0f73e3a1a75c9db4aab1a155
MyraMidnight <dark_stardragon@hotmail.com>
2018-12-09 21:50:10 +0000

    more comments

--- src/App.js
+++ src/App.js
@@ -18,11 +18,16 @@ class App extends Component {
     wpPages().then((pages)=>{
       store.dispatch(actions.getPages(pages));
     })
-    wpCategory().then((categories)=>{
+    /*wpCategory().then((categories)=>{
       store.dispatch(actions.getWpCategories(categories));
-    })
+    })*/
     //Array of category names, case sensitive
     wikiCategories(["Naggrísir", "Matur", "Búrið", "Heilsan"]).then((categories)=>{
+      //This will first fetch full list of existing categories
+      //it will compare the names of categories to see if defined categories exist on the wiki
+      //then it will continue to fetch the list of pages within existing categories
+      //then again fetch the contents of each page per existing category
+      //compiles it into a object that contains category title and array of pages
       store.dispatch(actions.getWikiCategories(categories));
     })
   }
